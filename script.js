6d8dcd104e87291eaf506ba4e1b5ddc22a1b7d6e
leprof <prof.violet.club@gmail.com>
2026-01-30 14:51:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // --- Chargement des membres ---
-async function chargerMembres() {
+async function chargerMembers() {
   try {
-    const response = await fetch("membres.json");
-    const membres = await response.json();
+    const response = await fetch("members.json");
+    const members = await response.json();
 
     // Trier par nom
-    membres.sort((a, b) => a.id.localeCompare(b.id));
+    members.sort((a, b) => a.id.localeCompare(b.id));
 
-    const container = document.getElementById("membres-container");
+    const container = document.getElementById("members-container");
     if (!container) return; // sécurité : ne fait rien si la page n'a pas ce conteneur
 
-    membres.forEach(membre => {
+    members.forEach(members => {
       const card = document.createElement("div");
       card.classList.add("card");
 
       card.innerHTML = `
-        <h3><a href=${membre.lien}>${membre.id}</a></h3>
+        <h3><a href=${members.lien}>${members.id}</a></h3>
       `;
 
       container.appendChild(card);
@@ -26,4 +26,4 @@ async function chargerMembres() {
 }
 
 // Lancer le chargement des membres uniquement si on est sur la page concernée
-document.addEventListener("DOMContentLoaded", chargerMembres);
+document.addEventListener("DOMContentLoaded", chargerMembers);
